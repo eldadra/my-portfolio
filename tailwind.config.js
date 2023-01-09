107d26fbed4b62715b231f1753884199f7e06abb
eldadra <eldad.rizaldi@ti.ukdw.ac.id>
2023-01-09 13:01:41 +0700

    update darkmode dan perbaikan fitur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['index.html'],
+  darkMode: 'class',
   theme: {
     container: {
       center: true,
@@ -11,7 +12,7 @@ module.exports = {
         primary: '#05386B',
         secondary: '#EDF5E1',
         tersier: '#64748b',
-        dark: '#000000'
+        dark: '#0f172a'
       },
       screens: {
         '2xl' : '1320px'
